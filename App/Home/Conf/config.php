cb8870377b40a74b26adfeada0ac1343d0b1db33
ericy <ericy@DOG>
2015-06-13 15:32:57 +0800

    smarty配置与测试结束，下一步准备测试mongodb数据库

--- App/Home/Conf/config.php
+++ App/Home/Conf/config.php
@@ -1,32 +1,44 @@
 <?php
-return array(
-    'APP_GROUP_LIST' => 'Home,Admin', // 分组
-    'DEFAULT_GROUP' => 'Home', // 默认分组
-    'APP_GROUP_MODE' => 1,
-    // '配置项'=>'配置值'
-    // 'DB_TYPE'=>'mysql',
-    // 'DB_HOST' => 'localhost',
-    // 'DB_NAME' => 'study',
-    // 'DB_USER' => 'root',
-    // 'DB_PWD' => '',
-    // 'DB_PORT' => '3306',
-    // 'DB_PREFIX' => '',
-    // 'SHOW_PAGE_TRACE' => true,
-    'DB_TYPE' => 'mongo', // 数据库类型
-    'DB_HOST' => 'localhost', // 服务器地址
-    'DB_NAME' => 'test', // 数据库名
-    'DB_USER' => '', // 用户名
-    'DB_PWD' => '', // 密码
-    'DB_PORT' => '27017', // 端口
+//     'APP_GROUP_LIST'		    => 'index,m,webapp', //分组
+//     'DEFAULT_GROUP'			    => 'index', //默认分组
+//     'DEFAULT_MODULE'		    => 'index', //默认控制器
+//     'TMPL_TEMPLATE_SUFFIX'  => '.phtml',
+//     'TAGLIB_PRE_LOAD'		    => 'fdc', //自动加载标签
+//     'APP_AUTOLOAD_PATH'		  => '@.Fdctag,@.Fdclib,@.ORG', //自动加载项目类库
+//     'TMPL_ACTION_SUCCESS'	  => '/public:success',
+//     'TMPL_ACTION_ERROR'		  => '/public:error',
     
-    'TMPL_ENGINE_TYPE' => 'Smarty',
-    'TMPL_TEMPLATE_SUFFIX' => '.phtml',
-    'TMPL_ENGINE_CONFIG' => array(
-        'caching' => false,
-        'template_dir' =>THEME_PATH,  //ThinkPHP.php定义了路径，否则报错，因为这个THEME_PATH未定义
-        'compile_dir' => TEMP_PATH,
-        'cache_dir' => CACHE_PATH,
-        'left_delimiter' => '{',
-        'right_delimiter' => '}',
-      ),
-);
+//     //调试时间
+//     'SHOW_RUN_TIME'    => false, // 运行时间显示
+//     'SHOW_ADV_TIME'    => false, // 显示详细的运行时间
+//     'SHOW_DB_TIMES'    => false, // 显示数据库查询和写入次数
+//     'SHOW_CACHE_TIMES' => false, // 显示缓存操作次数
+//     'SHOW_USE_MEM'     => false, // 显示内存开销
+//     'SHOW_LOAD_FILE'   => false, // 显示加载文件数
+//     'SHOW_FUN_TIMES'   => false, // 显示函数调用次数
+    
+//     //动态缓存
+//     'DATA_CACHE_TYPE'=>'File',//File、Apachenote、Apc、Eaccelerator、Memcache、Shmop、Sqlite、Db、Redis和Xcache。
+//     'DATA_CACHE_TIME'=>3600,
+//     'DATA_CACHE_SUBDIR'		  => false, //缓存文件夹
+//     // 静态缓存
+//     'HTML_CACHE_ON'=>true,
+//     'HTML_CACHE_TIME'=>21600,
+//     'HTML_FILE_SUFFIX'=>'.html',
+//     'HTML_NOT_CACHE'=>'user,share,union,gift,oauth,goods,banner,meigo,robots,backqndy,sign,inval,ajax,sea,forget,hot',
+//     'HTML_CACHE_RULES'=> array(
+//         '*'=>array('{$_SERVER.REQUEST_URI|md5}'),
+//     ),
+//     'DATA_PATH_LEVEL'		    => 3, //缓存文件夹层级
+//     'DEFAULT_FILTER'        => 'htmlspecialchars,strip_tags',
+//     'VAR_FILTERS'			      => 'filter_default,filter_exp',
+//     'LOAD_EXT_CONFIG'		    => 'url,db', //扩展配置
+//     'SHOW_PAGE_TRACE'		    => false,
+//     'APP_SUB_DOMAIN_DEPLOY' => true, // 开启子域名配置
+//     'APP_SUB_DOMAIN_RULES'  => array(
+//         'm'                     => array("m/"),
+//         'webapp'                => array("webapp/"),
+//         'adwbd'                => array("adwbd/"),
+//         '*'                		 => array("tdomain/"),
+//     ),
+// );
