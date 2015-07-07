9f582079313b704271a318c00d7a6f4944f1bb0d
Scott Chacon <root@ebs-25633.(none)>
2015-07-07 21:50:56 +0800

    d

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -1,10 +1,8 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-use Home\Model\MongoModel;
 class IndexController extends Controller{
    public function index(){
-   
      $Model =  D("User");
      // $Model->create();
      // $Model->name = '流年1';
